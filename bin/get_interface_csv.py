1f35e0a56d6b367784b0c8d7fd07cde36df4828d
ljunkie <github@linuxjunkie.com>
2012-09-06 02:47:19 -0700

    throughput typo and update to how we determing 64bit/32bit counters

--- bin/get_interface_csv.py
+++ bin/get_interface_csv.py
@@ -1,18 +1,31 @@
 #!/opt/zenoss/bin/python
 #
-# Utilized Python to export a list of device/interfaces from the 
+# Utilized Python to export a list of device/interfaces from the
 #
 import Globals, re, sys, Acquisition
 from Products.ZenUtils.ZenScriptBase import ZenScriptBase
 from transaction import commit
 
+from pprint import pprint
+from inspect import getmembers
 dmd = ZenScriptBase(connect=True).dmd
 
 for dev in dmd.Devices.getSubDevices():
-
+    #dev = dmd.Devices.findDevice('real device name') ## debug
+    #pprint(dump(dev))
     for int in dev.os.interfaces():
-        type = '32bit';
-        if re.search("_64", str(int.type)) :
-            type = '64bit'
+        # pprint(getmembers(int))
+        type = '32bit'; ## default to 32bit
+        template = dev.getRRDTemplateByName(int.type)
+        if template: # some don't have templates
+            for ds in template.getRRDDataSources():
+                if re.search("ifHC", str(ds.id)) :
+                    type = '64bit'
+                    #break
+        else: # for ints without their own template.. it probably defaulted back to <type> or <type>_64
+            if re.search("_64", str(int.type)) :
+                type = '64bit'            
+        print '"'+ dev.id + '","' + int.id + '","' + int.description + '","' + str(int.operStatus) + '","' + dev.title + '","' + type + '","' + int.type + '"'
 
-        print '"'+ dev.id + '","' + int.id + '","' + int.description + '","' + str(int.operStatus) + '","' + dev.title + '","' + type + '"'
+
+
